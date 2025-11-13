e982fd9b65cf4f4e8f137a99983e136b91179f2f
Md. Shakhawat Hossain Shahin <shakhawat.dev@gmail.com>
2025-11-13 17:40:54 +0600

    New Portfolio

---
+++ src/config.ts
@@ -0,0 +1,108 @@
+export const siteConfig = {
+  name: "MD Shakhawat Hossain Shahin",
+  title: "Senior iOS Developer",
+  description: "Portfolio website MD Shakhawat Hossain Shahin - Senior iOS Developer",
+  accentColor: "#04f610ff",
+  social: {
+    email: "shakhawat.dev@gmail.com",
+    linkedin: "https://linkedin.com/in/shakhawat-dev",
+    twitter: "https://x.com/Shakhawat_sms",
+    github: "https://github.com/shakhawat-dev",
+  },
+  aboutMe:
+    "Passionate and results-driven iOS Developer with years of experience who is dedicated to creating exceptional mobile applications through the implementation of clean and maintainable code. Focused on delivering Applications that are not only intuitive but also adhere to industry best practices. Currently learning cross platform development using Flutter. Expertise mainly on Native iOS Development with a deep understanding of Swift and SwiftUI but also have bit knowledge on Native Android and Flutter. Always aim for delivering quality large-scale applications for millions of users in mind. Participated in a few Hackathons and Programming competitions and had commendable results previously.",
+  skills: ["Swift", "SwiftUI", "UiKit", "Kotlin", "Combine", "Java", "Dart", "Flutter", "Git", "Python", "XML", "C++", "SQL", "HTML", "JSON", "C", "CSS", "MySQL", "Linux", "Firebase", "Bootstrap", "Android Studio", "Jira", "BitBucket", "Firebase", "Figma", "Adobe XD", "Postman"],
+  projects: [
+    {
+      name: "LIMS (Labour Information Management System)",
+      description:
+        "LIMS is a project for Bangladesh Labour Ministry to keep all the data of Labours employment and job history in a central Database. There will be a job portal to search jobs. All the Organization and Labour info will be saved on this central database.",
+      link: "https://apps.apple.com/us/app/lims/id6736583813",
+      skills: ["iOS 13", "Swift", "SwiftUI", "OAuth2", "Combine", "URLSession etc"],
+    },
+    {
+      name: "MBE Islamic Foundation",
+      description:
+        "Masjid-based children and mass education activities project Masjid-based Children and Mass Education Program project is one of the largest projects of Islamic Foundation with the aim of involving the imams of mosques in socio-economic development activities and education.",
+      link: "https://apps.apple.com/us/app/mbe-islamic-foundation/id6480442145",
+      skills: ["iOS 13", "Swift", "SwiftUI", "Maps", "Combine", "FaceID", "URLSession etc"],
+    },
+    {
+      name: "Stocker - Stock Exchange DSE",
+      description:
+        "Stocker is an Application for Share Holders stay up to date with Dhaka Stock Exchange. Used for tracking real-time prices, stay updated with market news, manage portfolios, and execute trades effortlessly.",
+      link: "https://apps.apple.com/us/app/stocker-stock-exchange-dse/id6445871145",
+      skills: ["Flutter", "Dart", "Provider"],
+    },
+  ],
+  experience: [
+    {
+      company: "SIMEC System Ltd.",
+      title: "iOS Developer",
+      dateRange: "Mar 2021 - Present",
+      bullets: [
+        "Developed LIMS, MBE - Islamic Foundation, MOCAT, Eilm, DSS-HRMS, DSS 85-SSP HRIS, Chaad Ride and other Govt. and Private projects",
+        "Learned Flutter and developed STOCKER app.",
+        "Learned Unity VR and Developed project for Oculus VR.",
+      ],
+    },
+    {
+      company: "Royal Green Ltd.",
+      title: "iOS Developer",
+      dateRange: "Aug 2020 - Mar 2021",
+      bullets: [
+        "Designed and Developed PACE NET and PACE Cloud iOS Application.",
+        "Implemented new Features to existing Applications.",
+        "Learned SwiftUI and Combine.",
+      ],
+    },
+    {
+      company: "AiSoftz Ltd.",
+      title: "iOS Developer",
+      dateRange: "Sep 2019 - Aug 2020",
+      bullets: [
+        "Developed Alama360 iOS Application Using UiKit.",
+        "Developed House Keeper Android Application.",
+        "Learned iOS Development using UiKit.",
+        "Bug Fixing and Support.",
+      ],
+    },
+    {
+      company: "Mensa DigiWorld Ltd.",
+      title: "Software Developer",
+      dateRange: "Oct 2018 - Aug 2019",
+      bullets: [
+        "Java Desktop Software Development.",
+        "Implement new Feature to existing Applications.",
+        "Learned iOS Development using UiKit.",
+        "Bug Fixing and Support.",
+      ],
+    },
+  ],
+  education: [
+    {
+      school: "Britannia University ",
+      degree: "Bachelor of Science in Computer Science",
+      dateRange: "2015 - 2021",
+      achievements: [
+        "NASA Space Apps Challenge – 2019 (Regional Winner)",
+        "Attended Google Android Study Jam 2016",
+        "Attended ACM ICPC Regional (Dhaka) 2018",
+        "NASA Space Apps Challenge – 2018 (Regional Runners-Up)",
+        "Attended National Hackathon 2016 - Dhaka Bangladesh",
+        "Vice President of Computer Club",
+        "Learning and Earning Development Project (LEDP) - 2017",
+      ],
+    },
+    {
+      school: "Google Developers Group Bangladesh",
+      degree: "Google Android Study Jam",
+      dateRange: "2016",
+      achievements: [
+        "Completed 30 days of coursework",
+        "Built 3+  projects",
+        "Specialized in Android Development",
+      ],
+    },
+  ],
+};
